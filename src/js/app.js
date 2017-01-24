ca2a177861ec9221848786a30ed5d0e7b656901d
yurie hamasaki <hamasaki@mediba.jp>
2017-01-24 17:49:31 +0900

    テストの西行ファイル追加

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import '../styl/main.styl';
+
+/*const fontColor = document.getElementById('content').style.color = 'brown';*/
+
+const ppap = 'ppap!!!!!!ppap!!!!!!ppap!!!!!!ppap!!!!!!ppap!!!!!!ppap!!!!!!';
+console.log(ppap);
